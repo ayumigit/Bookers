17712efe8b8029adff73901b0ffbb350c46f06c0
EC2 Default User <ec2-user@ip-172-31-37-17.ap-northeast-1.compute.internal>
2023-01-18 03:13:07 +0000

    kadai2-showpage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,35 @@
 class BooksController < ApplicationController
-  
+
   def index
-    @bookks = Book.all
+    @books = Book.all.order(id: :asc)
+
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+
+    if book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(book.id)
+    else
+      render :index
+    end
+  end
+  
+  def show
+    @book = Book.find(params[:id])  
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(book.id)
+  end
+ private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
