925c09c954e2ffe0d72c06f6fcbb05d876bea3b6
EC2 Default User <ec2-user@ip-172-31-37-17.ap-northeast-1.compute.internal>
2023-01-18 06:21:45 +0000

    kadai2-destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,17 +16,30 @@ class BooksController < ApplicationController
       render :index
     end
   end
-  
+
   def show
-    @book = Book.find(params[:id])  
+    @book = Book.find(params[:id])
   end
 
-  def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book.id)
+  def edit
+    @book = Book.find(params[:id])
   end
+
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+
+    flash[:notice] = "Book was successfully updated."
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
  private
   # ストロングパラメータ
   def book_params
